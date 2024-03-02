5f595b8872acf6b909113538f1c11c980c8f382f
Navid <navidadelpour@gmail.com>
2024-03-02 11:38:13 +0330

    fix: update social media links

--- src/consts/socialMedia.ts
+++ src/consts/socialMedia.ts
@@ -5,26 +5,25 @@ import InstagramIcon from "@/components/Icons/InstagramIcon";
 import LinkedinIcon from "@/components/Icons/LinkedInIcon";
 import TwitterIcon from "@/components/Icons/TwitterIcon";
 
-// TODO: fix the links.
 export const SOCIAL_MEDIAS: SocialMediaData[] = [
   {
     title: "Github",
     icon: GithubIcon,
     url: "https://github.com/navidadelpour",
   },
-  // {
-  //   title: "Linkedin",
-  //   icon: LinkedinIcon,
-  //   url: "https://github.com/navidadelpour",
-  // },
+  {
+    title: "Linkedin",
+    icon: LinkedinIcon,
+    url: "https://www.linkedin.com/in/navidadelpour/",
+  },
   {
     title: "Twitter",
     icon: TwitterIcon,
-    url: "https://github.com/navidadelpour",
+    url: "https://twitter.com/navidadelpour",
   },
   {
     title: "Instagram",
     icon: InstagramIcon,
-    url: "https://github.com/navidadelpour",
+    url: "https://www.instagram.com/navidadelpour/",
   },
 ];
